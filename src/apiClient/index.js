247989ebbf9147d9c724d92e8ded1290007417dc
Trevor B <t.brixey0129@gmail.com>
2020-04-30 02:52:30 -0500

    remove header stuff

--- src/apiClient/index.js
+++ src/apiClient/index.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
-
 export const apiClient = axios.create({
   baseURL: "http://localhost:3000",
 });
